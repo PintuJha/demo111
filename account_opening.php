bc43a3ac6184d0f2a76b78cd8c66337591b7b983
PINTU JHA <Pintu.Jha@motherson.com>
2024-03-20 10:34:59 +0530

    first commit

---
+++ account_opening.php
@@ -0,0 +1,67 @@
+<?php include 'common/header.php';?>
+<?php include 'common/menu.php';?>
+
+
+<style>
+.location_sec .panel .panel-body iframe {
+    width: 100%;
+    height: 300px;
+    border-radius: 12px 12px 0px 0px;
+}
+.location_sec .panel .panel-body {
+    padding: 0px;
+}
+.location_sec .panel .panel-body .address_div {
+    min-height: auto;
+    padding: 0px 15px 15px;
+}
+</style>
+
+
+<section class="search_sec text-center">
+	<div class="container">
+		<div>
+			<h3 class="bold" style="margin-bottom: 20px;">Account Opening  Video</h3>
+			<!-- <h4 class="sub_heading">Search for your nearest  NSDL Payments Bank CSP</h4> -->
+		
+		</div>
+	</div>
+</section>
+<section class="location_sec">
+	<div class="container">
+		<h4 class="bold mb25">Account Opening</h4>
+		<div class="row flex_wrap">
+				<div class="col-md-6 col-sm-12 col-xs-12 show-div">
+					<div class="panel">
+						<div class="panel-body ">
+                            <iframe src="https://www.youtube.com/embed/SyTuiOjYFFw?si=hoJciFab5aeQnAQG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <div class="address_div" >
+								<p class="address mt10"><a href="https://www.youtube.com/watch?v=SyTuiOjYFFw" target="_blank">Account Opening (English)</a></p>
+                          </div>
+						</div>
+					</div>
+				</div>
+
+
+                <div class="col-md-6 col-sm-12 col-xs-12 show-div">
+					<div class="panel">
+						<div class="panel-body">
+                            <iframe src="https://www.youtube.com/embed/j8p-K_-fmKQ?si=MornCmRTaafJj_mr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <div class="address_div" >
+								<p class="address mt10"><a href="https://www.youtube.com/watch?v=j8p-K_-fmKQ" target="_blank">Account Opening (Hindi)</a></p>
+                          </div>
+						</div>
+					</div>
+				</div>
+
+
+
+			
+		</div>
+
+	</div>
+</section>
+
+
+<?php include 'common/footer.php';?>
+<?php include 'common/js.php';?>
---
+++ footer.php
@@ -0,0 +1,197 @@
+<footer class="footer_sec section">
+  <div class="container">    
+    <div class="row link_ul">
+      <div class="col-sm-5">
+        <h1><img src="assets/layouts/images/logos/logo.png" alt="NSDL Logo"></h1>
+        <p>A license authorizing the bank to carry on payments bank business has been obtained from the Reserve Bank of India in terms of Section 22 of the Banking Regulation Act, 1949. It must be distinctly understood, however, that in issuing the license, the Reserve Bank of India does not undertake any responsibility for the financial soundness of the bank or for the correctness of any of the statements made or opinion expressed in this connection.</p>
+        
+        <p><b>THIS BANK IS PROTECTED BY</b></p>
+        <div class="dic_logo">
+        <img src="assets/layouts/images/logos/dic.jpg" alt="DIC GC">
+        <img src="assets/layouts/images/logos/qr_code.png" alt="QR Code">
+        </div>
+      </div>
+      <div class="col-sm-7">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <h5>Quick Access</h5>
+            <ul class="list-unstyled">
+              <li><a href="terms_and_conditions.php">Terms & Conditions</a></li>
+              <li><a href="offers_and_contests.php">Offers & Contests</a></li>
+              <li><a href="regulatory_disclosure.php">Regulatory Disclosure</a></li>
+              <li><a href="disclaimer.php">Disclaimer</a></li>              
+              <li><a href="https://www.nsdl.co.in/" target="_blank">NSDL Website</a></li>
+              <li><a href="https://rbidocs.rbi.org.in/rdocs/content/pdfs/BEAWARE07032022.pdf" target="_blank">Fraud Awareness</a></li>
+            </ul>
+
+
+            <h5 class="mt-2">Videos</h5>
+            <ul class="list-unstyled">
+               <li><a href="digital_banking_fraud_awareness.php">Digital Banking Fraud Awareness</a></li>
+               <li><a href="account_opening.php">Account opening</a></li>
+               <li><a href="debit_card.php">Debit card</a></li>
+            </ul>
+
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <h5>Policies</h5>
+            <ul class="list-unstyled">
+              <li><a href="privacy_policy.php">Privacy Policy</a></li>               
+              <!-- <li><a href="customer_service_and_grievance_redressal_policy.php">Customer Service and Grievance Redressal Policy</a></li> -->
+              <li><a href="assets/layouts/pdf/CustomerServiceAndGrievanceRedressalPolicy_V2.3.pdf" target="_blank">Customer Service and Grievance Redressal Policy</a></li>
+              <li><a href="assets/layouts/pdf/Customer_Deposit_Policy_03.05.2018.pdf" target="_blank">Deposit Policy</a></li>
+              <li><a href="assets/layouts/pdf/Customer_Protection_and_Compensation_Policy_NSDLPB_v1.3.pdf" target="_blank">Customer Protection and Compensation Policy</a></li>
+              <li><a href="whistleblower_policy.php">Whistleblower Policy</a></li>
+              <li><a href="assets/layouts/pdf/Citizen_Charter_NSDLPB.pdf" target="_blank">Citizen Charter</a></li>
+              <!-- <li><a href="assets/layouts/pdf/BCSBI-CodeofComittmentToCustomers.pdf" target="_blank">BCSBI Code of Commitment to Customers</a></li> -->
+              <li><a href="assets/layouts/pdf/ombudsman-scheme-for-digital-transactions-2019.pdf" target="_blank">Ombudsman Scheme for Digital Transactions, 2019</a></li>
+              <li><a href="assets/layouts/pdf/NSDLPB_Policy_on_Appointment_of_Statutory_Auditors.pdf" target="_blank">Policy for Appointment of Statutory Auditors (SAs)</a></li>
+              <li><a href="assets/layouts/pdf/Display_of_DICGC_Logo_and_QR_code_English.pdf" target="_blank">Display of DICGC Logo and QR Code by Insured Banks - English</a></li>
+              <li><a href="assets/layouts/pdf/Display_of_DICGC_Logo_and_QR_code_Hindi.pdf" target="_blank">Display of DICGC Logo and QR Code by Insured Banks - Hindi</a></li>
+          
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-12 col-xs-12">
+            <div class="row">
+              <div class="col-md-12 col-sm-6 col-xs-12">
+              <h5>Contact Us</h5>
+                <ul class="list-unstyled">
+                  <li><a href="tel:02242022100">022-42022100</a></li>
+                  <li><img style="margin-left: -2px;" src="assets/layouts/images/logos/carensdlbank_1.png" alt=""></li>
+                </ul>            
+              </div>
+              <div class="col-md-12 col-sm-6 col-xs-12">
+              <h5>Address</h5>
+                <ul class="list-unstyled">
+                  <li><p>NSDL Payments Bank <br>Limited, 401, 4th Floor, <br>Tower 3, One International <br>Center, Senapati Bapat Marg, Prabhadevi,<br>Mumbai - 400 013</p></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>            
+    </div>  
+    <div class="row copyright_txt mt15 pt15">
+      <div class="col-md-12"> 
+        <p class="bold beware_txt">Beware of fake Job offers made on behalf of NSDL Payments Bank. NSDL Payments Bank and any of its representatives never ask for any money for a job at the Bank or biometric device.</p> 
+        <p class="">Please do not share your UPI PIN, MPIN, OTP, Card details, CVV, ATM PIN, Card PIN, etc. with anyone, either over the phone or through email. NSDL Payments Bank never asks for it.</p>
+        <p>RBI never deals with individuals for Savings Account, Current Account, Credit Card, Debit Card, etc. Don’t be a victim of such offers coming to you on phone or email in the name of RBI.</p>    
+        <h5>Copyright © 2019 NSDL Payments Bank | All Rights Reserved.</h5>       
+      </div>
+    </div>
+  </div>
+</footer>
+
+<div class="slidebottom bg_white slide_popup_js">
+  <div class="pull-left">
+    <h4 class="bold">Cookies</h4>      
+  </div>
+  <div class="pull-right">     
+    <a class="closebtn slideClose_js">&times;</a>
+  </div>
+  <div class="cookie_text row">
+    <div class="col-md-11 col-sm-8">
+      <p>NSDL Payments Bank uses cookies and similar technologies to enhance your browsing experience and provide personalized recommendations. By using our online services, you consent the use of cookies in accordance with our <a href="privacy_policy.php">Cookie Policy</a></p>
+    </div>
+    <div class="col-md-1 col-sm-4">
+      <button class="btn accept_btn">Accept</button>
+    </div>
+  </div> 
+</div>
+
+
+
+<div class="overlay" style="display:none;"></div>
+
+<div id="qr_popup" class="modal fade center_popup download_app_pop" role="dialog">
+  <div class="modal-dialog modal-lg">    
+    <div class="modal-content">
+      <button type="button" class="close black" data-dismiss="modal">&times;</button>   
+      <div class="modal-body popup_bg">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 left_sec">
+              <div class="text-center">
+                <h3 class="bold mb20 font18">Open Your 100% Digital Bank Account on</h3>
+                <img src="assets/layouts/images/logos/nsdl_logo.png" class="img-responsive mx-auto">              
+              </div>
+              <div class="row app_download_images">
+                <div class="col-sm-12 text-center">
+                  <a class="btn btn_theme mb15 mt15 google_link" href="https://bit.ly/DNJ300920" onclick="return gtag_report_conversion('https://bit.ly/DNJ300920')" target="_blank">DOWNLOAD NOW</a>
+                  <p class="text-center medium mb15">OR</p>
+                  <div>
+                    <p class="bold">Scan this QR Code</p>
+                    <img src="assets/layouts/images/qrcodes/jiffy_qr.png" class="img-responsive" width="180">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xs-6 right_border">
+                  <img src="assets/layouts/images/qrcodes/google_play.png" class="img-responsive pull-right" width="120px">
+                </div>
+                <div class="col-xs-6">
+                  <img src="assets/layouts/images/qrcodes/app_store.png" class="img-responsive pull-left" width="120px">
+                </div>                
+              </div>
+            </div>
+            <div class="col-md-6 right_sec">
+              <img src="assets/layouts/images/qrcodes/NSDL-jiffy-Popup-original.png" class="img-responsive">
+            </div>         
+          </div>
+          <div class="row">
+            <div class="col-md-12 text-right mt15">
+              <p class="popup_disclaimer">A Product of <img src="assets/layouts/images/logos/logo.png" class="img-responsive popup"></p>
+            </div>
+          </div>
+        </div>
+      </div>     
+    </div>
+  </div>
+</div>
+
+<div id="start_popup" class="modal fade center_popup download_app_pop" role="dialog">
+  <div class="modal-dialog modal-lg">    
+    <div class="modal-content">
+      <button type="button" class="close black" data-dismiss="modal" id="redirect_home">&times;</button>   
+      <div class="modal-body popup_bg">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 left_sec">
+              <div class="text-center">
+                <h3 class="bold mb20 font18">Open Your 100% Digital Bank Account on</h3>
+                <img src="assets/layouts/images/logos/nsdl_logo.png" class="img-responsive mx-auto">              
+              </div>
+              <div class="row app_download_images">
+                <div class="col-sm-12 text-center">
+                  <a class="btn btn_theme mb15 mt15 google_link" href="https://jiffy.page.link/18_22" onclick="return gtag_report_conversion('https://jiffy.page.link/18_22')" target="_blank">DOWNLOAD NOW</a>
+                  <p class="text-center medium mb15">OR</p>
+                  <div>
+                    <p class="bold">Scan this QR Code</p>
+                    <img src="assets/layouts/images/qrcodes/campaign_qr.png" class="img-responsive" width="180">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xs-6 right_border">
+                  <img src="assets/layouts/images/qrcodes/google_play.png" class="img-responsive pull-right" width="120px">
+                </div>
+                <div class="col-xs-6">
+                  <img src="assets/layouts/images/qrcodes/app_store.png" class="img-responsive pull-left" width="120px">
+                </div>                
+              </div>
+            </div>
+            <div class="col-md-6 right_sec">
+              <img src="assets/layouts/images/qrcodes/NSDL-jiffy-Popup-original.png" class="img-responsive">
+            </div>         
+          </div>
+          <div class="row">
+            <div class="col-md-12 text-right mt15">
+              <p class="popup_disclaimer">A Product of <img src="assets/layouts/images/logos/logo.png" class="img-responsive popup"></p>
+            </div>
+          </div>
+        </div>
+      </div>     
+    </div>
+  </div>
+</div>
+
